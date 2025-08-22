76c7d9606a9a94c7f4b3f0eb5df134e53759a2f5
Watagua14 <164064575+Watagua14@users.noreply.github.com>
2025-08-22 11:16:13 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+// filepath: /workspaces/appCar/src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>¡Hola, AppCar!</h1>
+  </React.StrictMode>
+)
